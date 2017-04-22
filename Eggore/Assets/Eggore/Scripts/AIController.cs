e9dad0950b6d044892a07978964dea33cc6d6f2b
Kyle Lehtinen <kylelehtinen@gmail.com>
2017-04-22 14:57:06 -0400

    Collider and basic player tracking

---
+++ Eggore/Assets/Eggore/Scripts/AIController.cs
@@ -0,0 +1,41 @@
+﻿namespace Eggore
+{
+	using UnityEngine;
+
+	/// <summary>
+	/// Handles state logic for AI controlled entities
+	/// </summary>
+	[RequireComponent(typeof(CharacterMotor))]
+	public class AIController : MonoBehaviour
+	{
+		public enum State {IDLE, ATTACK};
+		private State activeState;
+
+		protected CharacterMotor motor;
+		protected GameObject player;
+
+		protected void Awake()
+		{
+			motor = GetComponent<CharacterMotor>();
+			foreach (Sensor sensor in GetComponentsInChildren<Sensor>())
+			{
+				sensor.ai = this;
+			}
+		}
+
+		protected void Update()
+		{
+			
+		}
+
+		public void onPlayerDetected(GameObject player)
+		{
+			motor.Destination = player.transform.position;
+		}
+
+		public void onPlayerEscape()
+		{
+			motor.Stop ();
+		}
+	}
+}
