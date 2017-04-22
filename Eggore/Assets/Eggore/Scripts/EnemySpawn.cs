155d4ae578be4c8e7e2cb85fd0a6e95624827d9f
Brian Marshburn <brian@redelfgames.com>
2017-04-22 15:29:51 -0400

    Added Enemy spawn

---
+++ Eggore/Assets/Eggore/Scripts/EnemySpawn.cs
@@ -0,0 +1,33 @@
+﻿namespace Eggore
+{
+    using UnityEngine;
+
+    public class EnemySpawn : MonoBehaviour
+    {
+
+        public GameObject[] enemyPrefabs;
+        public float spawnTime;
+
+        protected float elapsedTime;
+
+        protected void Spawn()
+        {
+            int index = Random.Range(0, enemyPrefabs.Length);            
+            GameObject enemy = Instantiate(enemyPrefabs[index]);
+            enemy.transform.position = transform.position;
+            enemy.transform.rotation = transform.rotation;
+        }
+
+        protected void Update()
+        {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > spawnTime)
+            {
+                Spawn();
+                elapsedTime -= spawnTime;
+            }
+        }
+
+    }
+
+}
