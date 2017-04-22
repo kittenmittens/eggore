37b6ae5656b9df93e8432d8e3a5e9321b0bac2d6
Brian Marshburn <brian@redelfgames.com>
2017-04-22 17:06:38 -0400

    Added Hit interface

---
+++ Eggore/Assets/Eggore/Scripts/IHittable.cs
@@ -0,0 +1,15 @@
+﻿namespace Eggore
+{
+    public enum HitType
+    {
+        None = 0,
+        Bullet,
+        Blast,
+    }
+
+    public interface IHittable
+    {
+        void OnHit(HitType hit, int damage);
+    }
+
+}
